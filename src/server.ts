63c14d931508a5b82160610f7657ff21dc2316c4
Lucas-Fabri <dev.cunhaww@gmail.com>
2026-03-20 13:43:32 -0300

    feat: add initial product routes and controller

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import { routes } from './routes/index';
 
 const PORT = 3333;
 const app = express();
 app.use(express.json());
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
