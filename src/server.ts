af04aeeb7f48c954dc5c538c91a038c6f42ab2b0
Lucas-Fabri <dev.cunhaww@gmail.com>
2026-03-24 09:55:01 -0300

    feat: add error middleware on server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import { routes } from './routes';
+import { errorHandlingMiddleware } from './middlewares/error-handling';
 
 const PORT = 3333;
 const app = express();
 app.use(express.json());
 app.use(routes);
+
+// Usando o Middleware de tratamento de erros, ele deve ser o último a ser registrado, para que ele capture os erros de todas as rotas e middlewares anteriores
+app.use(errorHandlingMiddleware);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
