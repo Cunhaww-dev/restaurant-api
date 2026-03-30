1441a9be6b071d2a9bad8f6ae58f7c5fb87306b7
Lucas-Fabri <dev.cunhaww@gmail.com>
2026-03-30 17:43:25 -0300

    feat: use typed insert payload in product controller

--- src/controllers/product-controller.ts
+++ src/controllers/product-controller.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { z } from 'zod';
+import { knex } from '@/database/knex';
+import { ProductInsert, ProductRow } from '@/database/types/product-types';
 
 class ProductController {
   // Métodos do controller
@@ -14,11 +16,15 @@ class ProductController {
   async create(request: Request, response: Response, next: NextFunction) {
     try {
       const bodySchema = z.object({
-        name: z.string().trim().min(5, 'Name must be at least 4 characters'),
+        name: z.string().trim().min(5, 'Name must be at least 5 characters'),
         price: z.number().gt(0, 'Price must be greater than 0'),
       });
 
       const { name, price } = bodySchema.parse(request.body); // parse lança um erro se a validação falhar
+
+      // tipando o objeto a ser inserido, omitindo os campos que são gerados automaticamente pelo banco de dados (id, created_at, updated_at)
+      const productToInsert: ProductInsert = { name, price };
+      await knex<ProductRow>('products').insert(productToInsert);
 
       return response.status(201).json({ name, price });
     } catch (error) {
