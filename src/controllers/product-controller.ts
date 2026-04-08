a20ed9690d3914098e1dc1b2d37af2bca02ab2ec
Lucas-Fabri <dev.cunhaww@gmail.com>
2026-04-08 17:22:52 -0300

    feat: add update method on controller

--- src/controllers/product-controller.ts
+++ src/controllers/product-controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { z } from "zod";
 import { knex } from "@/database/knex";
 import { ProductInsert, ProductRow } from "@/database/types/product-types";
+import { AppError } from '@/utils/AppError';
 
 class ProductController {
   // Listagem ordenada com filtro por nome
@@ -43,6 +44,42 @@ class ProductController {
       await knex<ProductRow>("products").insert(productToInsert);
 
       return response.status(201).json({ name, price });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async update(request: Request, response: Response, next: NextFunction) {
+    try {
+      // Validação dos parâmetros da requisição usando Zod'
+      const paramsSchema = z.object({
+        id: z.coerce.number().int().positive("ID must be a positive integer"),
+      });
+
+      // Validação do corpo da requisição para garantir que os dados estão corretos antes de tentar atualizar o produto
+      const bodySchema = z.object({
+        name: z.string().trim().min(5, "Name must be at least 5 characters"),
+        price: z.number().gt(0, "Price must be greater than 0"),
+      });
+
+      const { id } = paramsSchema.parse(request.params);
+      const { name, price } = bodySchema.parse(request.body);
+
+      // Atualização do produto no banco de dados usando Knex, retornando os dados atualizados para o cliente
+      const [updatedData] = await knex<ProductRow>("products")
+        .where({ id })
+        .update({ name, price, updated_at: knex.fn.now() })
+        .returning("*");
+
+        // Se o produto não for encontrado, retornamos um erro 404 para o cliente, indicando que o recurso solicitado não existe
+      if (!updatedData) {
+        throw new AppError("Product not found", 404);
+      }
+
+      return response.status(200).json({
+        message: "Product updated successfully",
+        product: updatedData,
+      });
     } catch (error) {
       next(error);
     }
