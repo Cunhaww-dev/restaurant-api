457ed31b2f85c0f6c82dc4acbdf268bd0ab8a549
Lucas-Fabri <dev.cunhaww@gmail.com>
2026-04-10 19:35:19 -0300

    feat: implement create table session functionality with validation

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -1,11 +1,48 @@
 import { Request, Response, NextFunction } from 'express';
+import { z } from 'zod';
+import { knex } from '@/database/knex';
+import { TableSessionRow } from '@/database/types/table-sessions-types';
+import { AppError } from '@/utils/AppError';
 
 export class TablesSessionsController {
   async create(request: Request, response: Response, next: NextFunction) {
     try {
-      return response
-        .status(201)
-        .json({ message: 'Table session created successfully' });
+      const bodySchema = z.object({
+        table_id: z.coerce
+          .number()
+          .int()
+          .positive('Table ID must be a positive integer'),
+      });
+
+      const { table_id } = bodySchema.parse(request.body);
+
+      const session = await knex<TableSessionRow>('tables_sessions')
+        .where({ table_id })
+        .whereNull('closed_at')
+        .first();
+
+      if (session && !session.closed_at) {
+        throw new AppError(
+          'There is already an open session for this table',
+          400,
+        );
+      }
+
+      const [createdSession] = await knex<TableSessionRow>('tables_sessions')
+        .insert({
+          table_id,
+          // opened_at: knex.fn.now(), não passamos porque a coluna no banco tem default para o timestamp atual
+        })
+        .returning('*');
+
+      if (!createdSession) {
+        throw new AppError('Table session not created', 400);
+      }
+
+      return response.status(201).json({
+        message: 'Table session created successfully',
+        session: createdSession,
+      });
     } catch (error) {
       next(error);
     }
