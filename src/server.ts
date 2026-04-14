d9eba89be469ddd798c304df1b57cc983232929d
Lucas-Fabri <dev.cunhaww@gmail.com>
2026-04-14 17:36:57 -0300

    feat: integrate Swagger UI for API documentation

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import { document } from './docs/openapi';
 import { routes } from './routes';
 import { errorHandlingMiddleware } from './middlewares/error-handling';
 
@@ -6,6 +8,7 @@ const PORT = 3333;
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(document));
 
 // Usando o Middleware de tratamento de erros, ele deve ser o último a ser registrado, para que ele capture os erros de todas as rotas e middlewares anteriores
 app.use(errorHandlingMiddleware);
