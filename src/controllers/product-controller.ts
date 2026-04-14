87b9d2d62cb52e6ef6bf940ab3721b0daca30b15
Lucas-Fabri <dev.cunhaww@gmail.com>
2026-04-13 23:05:15 -0300

    fix: add new validate for duplicated products name

--- src/controllers/product-controller.ts
+++ src/controllers/product-controller.ts
@@ -1,8 +1,8 @@
-import { NextFunction, Request, Response } from "express";
-import { z } from "zod";
-import { knex } from "@/database/knex";
-import { ProductInsert, ProductRow } from "@/database/types/product-types";
-import { AppError } from "@/utils/AppError";
+import { NextFunction, Request, Response } from 'express';
+import { z } from 'zod';
+import { knex } from '@/database/knex';
+import { ProductInsert, ProductRow } from '@/database/types/product-types';
+import { AppError } from '@/utils/AppError';
 
 class ProductController {
   // Listagem ordenada com filtro por nome
@@ -14,12 +14,12 @@ class ProductController {
       });
 
       const { name } = querySchema.parse(request.query);
-      const query = knex<ProductRow>("products")
+      const query = knex<ProductRow>('products')
         .select()
-        .orderBy("name", "asc");
+        .orderBy('name', 'asc');
 
       if (name) {
-        query.whereLike("name", `%${name}%`);
+        query.whereLike('name', `%${name}%`);
       }
 
       const products = await query;
@@ -33,15 +33,24 @@ class ProductController {
   async create(request: Request, response: Response, next: NextFunction) {
     try {
       const bodySchema = z.object({
-        name: z.string().trim().min(5, "Name must be at least 5 characters"),
-        price: z.number().gt(0, "Price must be greater than 0"),
+        name: z.string().trim().min(5, 'Name must be at least 5 characters'),
+        price: z.number().gt(0, 'Price must be greater than 0'),
       });
 
       const { name, price } = bodySchema.parse(request.body); // parse lança um erro se a validação falhar
 
+      const [productExists] = await knex<ProductRow>('products').whereRaw(
+        'LOWER(name) = ?',
+        [name.toLowerCase()],
+      );
+
+      if (productExists) {
+        throw new AppError('This product already exists', 400);
+      }
+
       // tipando o objeto a ser inserido, omitindo os campos que são gerados automaticamente pelo banco de dados (id, created_at, updated_at)
       const productToInsert: ProductInsert = { name, price };
-      await knex<ProductRow>("products").insert(productToInsert);
+      await knex<ProductRow>('products').insert(productToInsert);
 
       return response.status(201).json({ name, price });
     } catch (error) {
@@ -53,31 +62,40 @@ class ProductController {
     try {
       // Validação dos parâmetros da requisição usando Zod'
       const paramsSchema = z.object({
-        id: z.coerce.number().int().positive("ID must be a positive integer"),
+        id: z.coerce.number().int().positive('ID must be a positive integer'),
       });
 
       // Validação do corpo da requisição para garantir que os dados estão corretos antes de tentar atualizar o produto
       const bodySchema = z.object({
-        name: z.string().trim().min(5, "Name must be at least 5 characters"),
-        price: z.number().gt(0, "Price must be greater than 0"),
+        name: z.string().trim().min(5, 'Name must be at least 5 characters'),
+        price: z.number().gt(0, 'Price must be greater than 0'),
       });
 
       const { id } = paramsSchema.parse(request.params);
       const { name, price } = bodySchema.parse(request.body);
 
+      // Validação para garantir que não exista dois produtos com o mesmo nome
+      const [productWithSameName] = await knex<ProductRow>('products')
+        .whereRaw('LOWER(name) = ?', [name.toLowerCase()])
+        .whereNot('id', id); // "Ignora o meu próprio ID"
+
+      if (productWithSameName) {
+        throw new AppError('Another product already uses this name', 400);
+      }
+
       // Atualização do produto no banco de dados usando Knex, retornando os dados atualizados para o cliente
-      const [updatedData] = await knex<ProductRow>("products")
+      const [updatedData] = await knex<ProductRow>('products')
         .where({ id })
         .update({ name, price, updated_at: knex.fn.now() })
-        .returning("*");
+        .returning('*');
 
       // Se o produto não for encontrado, retornamos um erro 404 para o cliente, indicando que o recurso solicitado não existe
       if (!updatedData) {
-        throw new AppError("Product not found", 404);
+        throw new AppError('Product not found', 404);
       }
 
       return response.status(200).json({
-        message: "Product updated successfully",
+        message: 'Product updated successfully',
         product: updatedData,
       });
     } catch (error) {
@@ -88,22 +106,22 @@ class ProductController {
   async remove(request: Request, response: Response, next: NextFunction) {
     try {
       const paramsSchema = z.object({
-        id: z.coerce.number().int().positive("ID must be a positive integer"),
+        id: z.coerce.number().int().positive('ID must be a positive integer'),
       });
 
       const { id } = paramsSchema.parse(request.params);
 
-      const [deletedData] = await knex<ProductRow>("products")
+      const [deletedData] = await knex<ProductRow>('products')
         .where({ id })
         .delete()
-        .returning("*");
+        .returning('*');
 
       if (!deletedData) {
-        throw new AppError("Product not found", 404);
+        throw new AppError('Product not found', 404);
       }
 
       return response.status(200).json({
-        message: "Product deleted successfully",
+        message: 'Product deleted successfully',
         product: deletedData,
       });
     } catch (error) {
