70ed2fc230e8b4e207b97405992094d13371056e
Lucas-Fabri <dev.cunhaww@gmail.com>
2026-04-20 16:22:19 -0300

    fix: Swagger documentation for health, order, product, and table session paths

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,8 @@ import { errorHandlingMiddleware } from './middlewares/error-handling';
 const PORT = 3333;
 const app = express();
 app.use(express.json());
+app.use('/swagger', ...swaggerUi.serve, swaggerUi.setup(document));
 app.use(routes);
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(document));
 
 // Usando o Middleware de tratamento de erros, ele deve ser o último a ser registrado, para que ele capture os erros de todas as rotas e middlewares anteriores
 app.use(errorHandlingMiddleware);
